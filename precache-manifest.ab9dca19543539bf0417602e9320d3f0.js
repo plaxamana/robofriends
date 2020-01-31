008759abfabfafa7206cfc2c8265f22ec8c842aa
Philip Laxamana <philip.laxamana@hotmail.com>
2020-01-31 11:25:23 -0500

    Updates

---
+++ precache-manifest.ab9dca19543539bf0417602e9320d3f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c5d8618eb4a645917936f4688af6dd",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e80c600ad78460bf37e2",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6c943f78034795f1ea95",
+    "url": "/robofriends/static/css/main.9bbe903b.chunk.css"
+  },
+  {
+    "revision": "e80c600ad78460bf37e2",
+    "url": "/robofriends/static/js/2.d6fca685.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.d6fca685.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c943f78034795f1ea95",
+    "url": "/robofriends/static/js/main.f96015f6.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
